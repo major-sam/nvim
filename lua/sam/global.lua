cb1fb3a295165aaf946ec0fbe7b8be6584adda5c
vrebyachih <vrebyachih@baltbet.ru>
2023-12-19 12:00:02 +0300

    arrow

--- lua/sam/global.lua
+++ lua/sam/global.lua
@@ -35,7 +35,7 @@ vim.g.NERDTreeExactMatchHighlightFullName = 1
 vim.g.NERDTreePatternMatchHighlightFullName = 1
 
 vim.g.NERDTreeDirArrowExpandable = '+'
-vim.g.NERDTreeDirArrowCollapsible = ' '
+vim.g.NERDTreeDirArrowCollapsible = '➤'
 vim.g.nerdtree_vis_confirm_open = 0
 vim.g.nerdtree_vis_confirm_delete = 1
 vim.g.nerdtree_vis_confirm_copy = 0
