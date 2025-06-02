a3e2b1459027531bb0c4888612f3743d8790f433
vrebyachih <vrebyachih@baltbet.ru>
2025-06-02 14:53:41 +0300

    telescope file browser

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,18 @@
 local builtin = require('telescope.builtin')
 
+
+require("telescope").load_extension("file_browser")
 require("telescope").load_extension("yaml_schema")
 require('telescope').load_extension('nerdy')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
+vim.keymap.set("n", "<F3>", ":Telescope file_browser<CR>")
 
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 local harpoon = require('harpoon')
@@ -17,20 +21,20 @@ harpoon:setup({})
 -- basic telescope configuration
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
+  require("telescope.pickers").new({}, {
+    prompt_title = "Harpoon",
+    finder = require("telescope.finders").new_table({
+      results = file_paths,
+    }),
+    previewer = conf.file_previewer({}),
+    sorter = conf.generic_sorter({}),
+  }):find()
 end
 
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
+  { desc = "Open harpoon window" })
--- lua/sam/plugins.lua
+++ lua/sam/plugins.lua
@@ -1,6 +1,4 @@
 require("lazy").setup({
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
   "jeetsukumaran/vim-indentwise",
   {
@@ -190,30 +188,34 @@ require("lazy").setup({
     end
   },
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function ()
-      -- If you want icons for diagnostic errors, you"ll need to define them somewhere:
-      vim.fn.sign_define("DiagnosticSignError",
-        {text = " ", texthl = "DiagnosticSignError"})
-      vim.fn.sign_define("DiagnosticSignWarn",
-        {text = " ", texthl = "DiagnosticSignWarn"})
-      vim.fn.sign_define("DiagnosticSignInfo",
-        {text = " ", texthl = "DiagnosticSignInfo"})
-      vim.fn.sign_define("DiagnosticSignHint",
-        {text = "󰌵", texthl = "DiagnosticSignHint"})
-    end
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   },
+--  {
+--    "nvim-neo-tree/neo-tree.nvim",
+--    branch = "v3.x",
+--    dependencies = {
+--      "nvim-lua/plenary.nvim",
+--      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+--      "MunifTanjim/nui.nvim",
+--      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+--    },
+--    config = function ()
+--      -- If you want icons for diagnostic errors, you"ll need to define them somewhere:
+--      vim.fn.sign_define("DiagnosticSignError",
+--        {text = " ", texthl = "DiagnosticSignError"})
+--      vim.fn.sign_define("DiagnosticSignWarn",
+--        {text = " ", texthl = "DiagnosticSignWarn"})
+--      vim.fn.sign_define("DiagnosticSignInfo",
+--        {text = " ", texthl = "DiagnosticSignInfo"})
+--      vim.fn.sign_define("DiagnosticSignHint",
+--        {text = "󰌵", texthl = "DiagnosticSignHint"})
+--    end
+--  },
   -- LSP Support
   "neovim/nvim-lspconfig",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
+  "mason-org/mason.nvim",
+  "mason-org/mason-lspconfig.nvim",
 
   -- Autocompletion
   {
@@ -252,6 +254,8 @@ require("lazy").setup({
     }
   },
   "laytan/cloak.nvim",
+  "PhilRunninger/nerdtree-buffer-ops",
+  "PhilRunninger/nerdtree-visual-selection",
   -- Obsidian
   {
     "epwalsh/obsidian.nvim",
