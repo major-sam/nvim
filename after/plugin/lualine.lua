a3275fd8dccbd62c1acf695942343a17056f73b6
vrebyachih <vrebyachih@baltbet.ru>
2025-11-19 11:49:30 +0300

    *

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,3 @@
-local Harpoonline = require("harpoonline")
-Harpoonline.setup({
-  on_update = function() require("lualine").refresh() end,
-})
 require('lualine').setup{
   options = {
     icons_enabled = true,
@@ -24,29 +20,6 @@ require('lualine').setup{
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {
-      Harpoonline.format,
-      {
-        'filename',
-        file_status = true,      -- Displays file status (readonly status, modified status)
-        newfile_status = false,  -- Display new file status (new file means no write after created)
-        path = 3,
-        -- 0: Just the filename
-        -- 1: Relative path
-        -- 2: Absolute path
-        -- 3: Absolute path, with tilde as the home directory
-        -- 4: Filename and parent dir, with tilde as the home directory
-
-        shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-        -- for other components. (terrible name, any suggestions?)
-        symbols = {
-          modified = '[+]',      -- Text to show when the file is modified.
-          readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-          unnamed = '[No Name]', -- Text to show for unnamed buffers.
-          newfile = '[New]',     -- Text to show for new
-        }
-      },
-    },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {
