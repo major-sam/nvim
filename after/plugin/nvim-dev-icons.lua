9e09b3afccb6a3d23f7ebe05014176a0e5c3094b
vrebyachih <vrebyachih@baltbet.ru>
2026-07-15 16:10:24 +0300

    **

--- after/plugin/nvim-dev-icons.lua
+++ after/plugin/nvim-dev-icons.lua
@@ -18,8 +18,8 @@ require 'nvim-web-devicons'.setup {
   strict = true,
   override_by_filename = {
     ["Dockerfile"] = { icon = "", color = "#0034c3", cterm_color = "4", name = "Docker" },
-    ["Jenkinsfile"] = { icon = "", cterm_color = "196", color = "#FF4500", name = "Jenkins" },
-    ["JenkinsFile"] = { icon = "", cterm_color = "196", color = "#FF4500", name = "Jenkins" },
-    ["jenkinsfile"] = { icon = "", cterm_color = "196", color = "#FF4500", name = "Jenkins" },
+    ["Jenkinsfile"] = { icon = "", color = "#FF4500", name = "Jenkins" },
+    ["JenkinsFile"] = { icon = "", color = "#FF4500", name = "Jenkins1" },
+    ["jenkinsfile"] = { icon = "", color = "#FF4500", name = "Jenkins2" },
   }
 };
