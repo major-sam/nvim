461431803d643a3a1921fc8a184bff5ea46cb2e1
vrebyachih <vrebyachih@baltbet.ru>
2026-09-04 11:41:37 +0300

    swap to dup python

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,3 @@
-local dap = require('dap')
 local signs = {
   DapBreakpoint          = { text = '🐞', texthl = 'DapBreakpoint', linehl = '', numhl = '' },
   DapBreakpointCondition = { text = '❓', texthl = 'DapBreakpointCondition', linehl = '', numhl = '' },
@@ -10,17 +9,33 @@ for name, sign in pairs(signs) do
   vim.fn.sign_define(name, sign)
 end
 
+local dap = require('dap')
 dap.defaults.fallback.terminal_win_cmd = 'tabnew'
 dap.defaults.fallback.focus_terminal = true
 
-dap.configurations.python = {
-  {
-    -- The first three options are required by nvim-dap
-    type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
-    request = 'launch',
-    name = "Launch file",
 
-    program = "${file}", -- This configuration will launch the current file if used.
+dap_python = require('dap-python')
 
-  },
-}
+local cwd = vim.fn.getcwd()
+if string.lower(jit.os) == 'windows' then
+  if vim.fn.executable(cwd .. '/venv/Scripts/python.exe') == 1 then
+    dap_python.setup(cwd .. './venv/Scripts/python.exe')
+  elseif vim.fn.executable(cwd .. '/venv/Scripts/python.exe') == 1 then
+    dap_python.setup(cwd .. './.venv/Scripts/python.exe')
+  else
+    dap_python.setup(vim.g.python3_host_prog)
+  end;
+else
+  print ('linux')
+  if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+    dap_python.setup(cwd .. '/venv/bin/python')
+  elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+    dap_python.setup(cwd .. '/.venv/bin/python')
+  else
+    dap_python.setup('python3')
+  end;
+end;
+if string.lower(jit.os) == 'windows' then
+  require("dap-python").setup(vim.g.python3_host_prog)
+else
+end;
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -40,12 +40,6 @@ return {
       -- This line is essential to making automatic installation work
       -- :exploding-brain
       handlers = {},
-      automatic_installation = {
-        -- These will be configured by separate plugins.
-        exclude = {
-          "python",
-        },
-      },
       -- DAP servers: Mason will be invoked to install these if necessary.
       ensure_installed = {
         "bash",
